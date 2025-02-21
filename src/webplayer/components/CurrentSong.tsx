3011dfd86efdc5940a6863b8a1b185541ba1e90d
Carlos Carrete <carloscarrete.sc@gmail.com>
2025-02-20 21:12:48 -0600

    `Added heartbeat animation to favorite button and improved CurrentSong component`

--- src/webplayer/components/CurrentSong.tsx
+++ src/webplayer/components/CurrentSong.tsx
@@ -1,18 +1,44 @@
 import { Heart } from "lucide-react"
 import usePlayerStore from "../../store/useWebPlayer";
+import { useEffect, useState } from "react";
 
 export const CurrentSong = () => {
 
     const { tracks, currentTrackIndex, setFavoriteCurrentTrack } = usePlayerStore();
     const currentTrack = tracks[currentTrackIndex];
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const handleFavorite = () => {
+        if (!currentTrack) return;
+        setIsAnimating(true);
+        setFavoriteCurrentTrack(currentTrack._id)
+    }
+
+    useEffect(() => {
+        if (isAnimating) {
+            setTimeout(() => {
+                setIsAnimating(false);
+            }, 600);
+        }
+    }, [isAnimating])
+
 
     return (
         <div className="flex gap-3 w-80 items-center">
             <img src={currentTrack?.cover} alt="Cover" className="w-10 h-10 rounded-md object-cover" />
-            <div>
-                <p className="font-bold">{currentTrack?.name}</p>
+            <div className="overflow-hidden w-3/5">
+                <p className="font-bold truncate hover:animate-scroll">{currentTrack?.name}</p>
                 <p className="text-sm text-zinc-400">{currentTrack?.artist.name}</p>
             </div>
-            <Heart onClick={() => setFavoriteCurrentTrack(currentTrack?._id)} size={24} fill="red" stroke="red" className="cursor-pointer text-white ml-3 hover:text-red-600" />
+            <Heart
+                onClick={handleFavorite}
+                size={24}
+                fill={currentTrack?.favorite ? "red" : "none"}
+                stroke="red"
+                className={`cursor-pointer text-white ml-3 hover:text-red-600 ${isAnimating ? 'animate-heartbeat' : ''}`}
+            />
         </div>)
 }
+
+
+
