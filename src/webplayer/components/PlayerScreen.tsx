fee58cefcdd7ed9cbe5a56ebdae5ac5ee529ad77
Carlos Carrete <carloscarrete.sc@gmail.com>
2025-02-21 14:18:50 -0600

    `Refactor grid layout and image container in PlayerScreen component`

--- src/webplayer/components/PlayerScreen.tsx
+++ src/webplayer/components/PlayerScreen.tsx
@@ -15,12 +15,12 @@ export const PlayerScreen = ({tracks}: Props) => {
     return (
         <main className="bg-black flex-1 p-2">
             <SearchInput />
-            <div className="bg-zinc-900 flex h-screen rounded-xl p-4">
-                <div className="grid grid-cols-5 md:grid-cols-4 sm:grid-cols-2 lg:grid-cols-5 gap-6 auto-rows-min">
+            <div className="bg-zinc-900  h-screen rounded-xl p-4">
+                <div className="grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-6 auto-rows-min">
+                {/* <div className="grid grid-cols-5 md:grid-cols-4 sm:grid-cols-2 lg:grid-cols-5 gap-6 auto-rows-min"> */}
                     {displayedTracks.map((track, index) => (
                         <div key={track._id} className="relative group overflow-hidden" onClick={()=>setCurrentTrackIndex(track._id)}>
-                            {/* Contenedor para la imagen con transición */}
-                            <div className="transform transition-all duration-300 ease-in-out group-hover:scale-110">
+                            <div className="transform transition-all duration-300 ease-in-out group-hover:scale-110 flex items-center justify-center overflow-hidden">
                                 <img 
                                     src={track.cover} 
                                     alt={track.name} 
@@ -28,8 +28,7 @@ export const PlayerScreen = ({tracks}: Props) => {
                                 />
                             </div>
                             
-                            {/* Overlay con el nombre */}
-                            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/90 to-black/40 p-3 opacity-0 transition-all group-hover:opacity-100">                                <p className="text-white text-sm font-medium text-center truncate ">
+                            <div className="overflow-hidden absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/90 to-black/40 p-3 opacity-0 transition-all group-hover:opacity-100">                                <p className="text-white text-sm font-medium text-center truncate ">
                                     {track.name}
                                 </p>
                             </div>
