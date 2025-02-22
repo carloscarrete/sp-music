d5f5f66aa9543a5186a6ce64eeff1fd62cdab26f
Carlos Carrete <carloscarrete.sc@gmail.com>
2025-02-21 18:02:17 -0600

    `Update SidebarNav component to display user name and fetch user data from useAuthStore`

--- src/webplayer/components/SidebarNav.tsx
+++ src/webplayer/components/SidebarNav.tsx
@@ -5,11 +5,11 @@ import { useAuthStore } from '../../store/useAuthStore';
 export const SidebarNav = () => {
 
     const { setShowSearch, showSearch, setSearchQuery, } = usePlayerStore();
-    const {logout} = useAuthStore();
+    const {logout, user} = useAuthStore();
 
     return (
         <nav className="bg-zinc-900 p-4 flex flex-col gap-1.5 rounded-xl">
-            <h1>Bienvenido: 'username'</h1>
+            <p>Bienvenido, <strong>{user?.name}</strong></p>
             <button className="text-zinc-200 flex items-center gap-4 text-lg hover:text-white" onClick={() => { setSearchQuery(''), setShowSearch(false) }}><Home size={24} />Inicio</button>
             <button className="text-zinc-200 flex items-center gap-4 text-lg hover:text-white" onClick={() => setShowSearch(!showSearch)}><Search size={24} />Buscar</button>
             <button className="text-zinc-200 flex items-center gap-4 text-lg hover:text-red-600" onClick={logout}> <LogOut /> Cerrar sesión </button>
